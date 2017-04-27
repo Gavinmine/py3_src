a9c31184dfe20f31f3446b515c7b0a44868bff7d
Gavin.Lin <whatever.2099@gmail.com>
2017-04-27 23:21:47 +0800

    can work now

--- spiders/douban/douban/settings.py
+++ spiders/douban/douban/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'douban.spiders'
 #USER_AGENT = 'douban (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -89,12 +89,28 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
+# HEADER = {
+#         "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+#         "Accept-Encoding":"gzip, deflate, sdch",
+#         "Accept-Language":"zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4,ja;q=0.2,de;q=0.2",
+#         "Connection":"keep-alivei",
+#         "Host":"douban.com",
+#         "Upgrade-Insecure-Requests":"1",
+#         "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36",
+#         }
+
 HEADER = {
-        "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "Accept-Encoding":"gzip, deflate, sdch",
-        "Accept-Language":"zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4,ja;q=0.2,de;q=0.2",
-        "Connection":"keep-alivei",
-        "Host":"douban.com",
-        "Upgrade-Insecure-Requests":"1",
-        "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36",
-        }
+    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding":"gzip, deflate, sdch, br",
+    "Accept-Language":"zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4,ja;q=0.2,de;q=0.2",
+    "Cache-Control":"max-age=0",
+    "Connection":"keep-alive",
+    "Host":"movie.douban.com",
+    "Referer":"https://www.douban.com/",
+    "Upgrade-Insecure-Requests":"1",
+    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36",
+}
+
+COOKIES = {
+    "Cookie":'bid=oGf7e-Q4iYI; ll="108300"; ap=1; _is_first_access=user1493291598; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1493305147%2C%22https%3A%2F%2Fwww.douban.com%2Fsearch%3Fq%3Dthis%2Bis%2Bus%22%5D; __utmt=1; _pk_id.100001.4cf6=6b5547e54b9226a1.1483847431.6.1493305903.1493291692.; _pk_ses.100001.4cf6=*; __utma=30149280.941994995.1483847362.1493291541.1493305147.15; __utmb=30149280.1.10.1493305147; __utmc=30149280; __utmz=30149280.1491733406.12.11.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utma=223695111.417144819.1483847431.1493291541.1493305147.6; __utmb=223695111.0.10.1493305147; __utmc=223695111; __utmz=223695111.1486563873.3.3.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/search; _vwo_uuid_v2=9EC51B92DA47901FD9F4B2FA6742A923|313754330d6bec8511d45d5552c08124',
+}
--- spiders/douban/douban/spiders/douban_spider.py
+++ spiders/douban/douban/spiders/douban_spider.py
@@ -1,18 +1,29 @@
 import scrapy
+from scrapy.http import Request,FormRequest
 from douban.settings import *
 
 class doubanSpider(scrapy.Spider):
     name = "douban"
-    allowed_domains = ["move.douban.com"]
+    allowed_domains = ["douban.com"]
     start_urls = [
-            "https://movie.douban.com/subject/26260853/",
-            "https://movie.douban.com/subject/7065334/"
+            "https://www.douban.com/subject/26260853/",
+            "https://www.douban.com/subject/7065334/"
         ]
 
     def __init__(self):
-        self.headers =HEADER
+        self.headers = HEADER
+        self.cookies = COOKIES
+
+    def start_requests(self):
+        for i, url in enumerate(self.start_urls):
+            yield FormRequest(url, meta={'cookiejar': i}, \
+                              headers=self.headers, \
+                              cookies=self.cookies, \
+                              callback=self.parse, \
+                              )
 
     def parse(self, response):
         filename = response.url.split("/")[-2]
+        print("filename:%s\n\n\n\n\n\n", filename)
         with open(filename, 'wb') as f:
             f.write(response.body)
